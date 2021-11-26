de7e4110fea2475d4704ab9710777dedc793fc5e
Cleiton <cleiton.araujo.farias@gmail.com>
2021-11-26 15:49:49 -0300

    feat: created structure initial

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  console.log('app');
+  return (
+    <div className="App">
+      <span>App</span>
+    </div>
+  );
+};
+
+export default App;
