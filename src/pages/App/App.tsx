6be900fdda3bebd4e1edcfc56ac2244b6ba32934
Cleiton <cleiton.araujo.farias@gmail.com>
2021-11-26 18:42:18 -0300

    feat: added getTools service in App component

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// Services
+import { getTools } from 'services/VuttrService';
+
+// Semantic UI components
+import { Button } from 'semantic-ui-react';
+
+// Models
+import { ToolEntity } from 'models/toolEntity';
+
+// Styles
 import './App.scss';
 
 const App: React.FC = () => {
-  console.log('app');
+  const [tools, setTools] = useState<ToolEntity[]>([]);
+
+  const fetchTools = async () => {
+    try {
+      const response = await getTools();
+      setTools(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTools();
+  }, []);
+
   return (
     <div className="App">
-      <span>App</span>
+      <ol>
+        {tools.map((tool) => (
+          <ul key={tool.id}>{tool.description}</ul>
+        ))}
+      </ol>
+      <Button>Entrar</Button>
     </div>
   );
 };
